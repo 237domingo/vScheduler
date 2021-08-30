c01bac640f85fa4fe32fd2495681fb1801922896
237domingo <85198491+237domingo@users.noreply.github.com>
2021-08-30 14:24:53 +0100

    Change v number

--- vController/Properties/AssemblyInfo.cs
+++ vController/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("35790407-a5de-4d5a-8280-1f6ac55b6cd4")]
-[assembly: AssemblyVersion("1.1.4.0")]
-[assembly: AssemblyFileVersion("1.1.4.0")]
+[assembly: AssemblyVersion("1.1.4.3")]
+[assembly: AssemblyFileVersion("1.1.4.3")]
 [assembly: NeutralResourcesLanguageAttribute("")]
